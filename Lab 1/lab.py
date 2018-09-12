c4d95bb5e685d5a332526e992b46ae37864e0fef
biedn001 <biedn001@umn.edu>
2018-09-11 19:40:20 -0500

    more solving functions

--- Lab 1/lab.py
+++ Lab 1/lab.py
@@ -19,7 +19,52 @@ def solve(v, q):
         return solving(v, flippedQ)
     else:
         return None
-    
 
 def solving(v, q):
-    #whatever
+    #v/x will be in the left side, middle will be =, right will be something
+    if v == q[0]:
+        return q # already solved
+    elif q[0][1] == "+":
+        return solvingAddition(v, q)
+    elif q[0][1] == "-":
+        return solvingSubtract(v, q)
+    elif q[0][1] == "*":
+        return solvingAddition(v, q)
+    elif q[0][1] == "/":
+        return solvingDivide(v, q)
+    
+def solvingAddition(v, q):
+    if isInside(v, q[0][0]):
+        #x is on the left of the left of the equation
+        newThing = (q[0][0], '=', (q[2], '-', q[0][2]))
+    elif isInside(v, q[0][2]):
+        #x is on the right of the left of the equation
+        newThing = (q[0][2], '=', (q[2], '-', q[0][0]))
+    solving(v, newThing)
+
+def solvingSubtract(v, q):
+    if isInside(v, q[0][0]):
+        #x is on the left of the left of the equation
+        newThing = (q[0][0], '=', (q[2], '+', q[0][2]))
+    elif isInside(v, q[0][2]):
+        #x is on the right of the left of the equation
+        newThing = (q[0][2], '=', (q[0][0], '-', q[2]))
+    solving(v, newThing)
+
+def solvingMultiply(v, q):
+    if isInside(v, q[0][0]):
+        #x is on the left of the left of the equation
+        newThing = (q[0][0], '=', (q[2], '/', q[0][2]))
+    elif isInside(v, q[0][2]):
+        #x is on the right of the left of the equation
+        newThing = (q[0][2], '=', (q[2], '/', q[0][0]))
+    solving(v, newThing)
+
+def solvingDivide(v, q):
+    if isInside(v, q[0][0]):
+        #x is on the left of the left of the equation
+        newThing = (q[0][0], '=', (q[2], '*', q[0][2]))
+    elif isInside(v, q[0][2]):
+        #x is on the right of the left of the equation
+        newThing = (q[0][2], '=', (q[0][0], '/', q[2]))
+    solving(v, newThing)

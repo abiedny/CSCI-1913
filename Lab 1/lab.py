262b3557994b351b25da6ef5383b04d93809df0a
biedn001 <biedn001@umn.edu>
2018-09-12 12:22:52 -0500

    fixed imports and solved the lab

--- Lab 1/lab.py
+++ Lab 1/lab.py
@@ -29,7 +29,7 @@ def solving(v, q):
     elif q[0][1] == "-":
         return solvingSubtract(v, q)
     elif q[0][1] == "*":
-        return solvingAddition(v, q)
+        return solvingMultiply(v, q)
     elif q[0][1] == "/":
         return solvingDivide(v, q)
     
@@ -68,43 +68,3 @@ def solvingDivide(v, q):
         #x is on the right of the left of the equation
         newThing = (q[0][2], '=', (q[0][0], '/', q[2]))
     return solving(v, newThing)
-
-print(isInside('x', 'x'))                          #  True   1 point
-print(isInside('x', 'y'))                          #  False  1 point
-print(isInside('x', ('x', '+', 'y')))              #  True   2 points
-print(isInside('x', ('a', '+', 'b')))              #  False  2 points
-print(isInside('+', ('a', '+', 'b')))              #  False  2 points
-print(isInside('x', (('m', '*', 'x'), '+', 'b')))  #  True   2 points
-
-print(solve('x', (('a', '+', 'x'), '=', 'c')))
-#  ('x', '=', ('c', '-', 'a'))  2 points
-
-print(solve('x', (('x', '+', 'b'), '=', 'c')))
-#  ('x', '=', ('c', '-', 'b'))  2 points
-
-print(solve('x', (('a', '-', 'x'), '=', 'c')))
-#  ('x', '=', ('a', '-', 'c'))  2 points
-
-print(solve('x', (('x', '-', 'b'), '=', 'c')))
-#  ('x', '=', ('c', '+', 'b'))  2 points
-
-print(solve('x', (('a', '*', 'x'), '=', 'c')))
-#  ('x', '=', ('c', '/', 'a'))  2 points
-
-print(solve('x', (('x', '*', 'b'), '=', 'c')))
-#  ('x', '=', ('c', '/', 'b'))  2 points
-
-print(solve('x', (('a', '/', 'x'), '=', 'c')))
-#  ('x', '=', ('a', '/', 'c'))  2 points
-
-print(solve('x', (('x', '/', 'b'), '=', 'c')))
-#  ('x', '=', ('c', '*', 'b'))  2 points
-
-print(solve('y', ('y', '=', (('m', '*', 'x'), '+', 'b'))))
-# ('y', '=', (('m', '*', 'x'), '+', 'b'))  2 points
-
-print(solve('x', ('y', '=', (('m', '*', 'x'), '+', 'b'))))
-# ('x', '=', (('y', '-', 'b'), '/', 'm'))  2 points
-
-print(solve('a', (('b', '+', 'c'), '=', ('d', '*', (('a', '/', 'e'), '-', 'f')))))
-# ('a', '=', (((('b', '+', 'c'), '/', 'd'), '+', 'f'), '*', 'e'))  5 points
--- Lab 1/tests.py
+++ Lab 1/tests.py
@@ -8,44 +8,45 @@
 #  code works correctly. It also shows how many points the test is worth, for a
 #  total of 35 possible points.
 #
+import types
 import lab
 
-print(isInside('x', 'x'))                          #  True   1 point
-print(isInside('x', 'y'))                          #  False  1 point
-print(isInside('x', ('x', '+', 'y')))              #  True   2 points
-print(isInside('x', ('a', '+', 'b')))              #  False  2 points
-print(isInside('+', ('a', '+', 'b')))              #  False  2 points
-print(isInside('x', (('m', '*', 'x'), '+', 'b')))  #  True   2 points
+print(lab.isInside('x', 'x'))                          #  True   1 point
+print(lab.isInside('x', 'y'))                          #  False  1 point
+print(lab.isInside('x', ('x', '+', 'y')))              #  True   2 points
+print(lab.isInside('x', ('a', '+', 'b')))              #  False  2 points
+print(lab.isInside('+', ('a', '+', 'b')))              #  False  2 points
+print(lab.isInside('x', (('m', '*', 'x'), '+', 'b')))  #  True   2 points
 
-print(solve('x', (('a', '+', 'x'), '=', 'c')))
+print(lab.solve('x', (('a', '+', 'x'), '=', 'c')))
 #  ('x', '=', ('c', '-', 'a'))  2 points
 
-print(solve('x', (('x', '+', 'b'), '=', 'c')))
+print(lab.solve('x', (('x', '+', 'b'), '=', 'c')))
 #  ('x', '=', ('c', '-', 'b'))  2 points
 
-print(solve('x', (('a', '-', 'x'), '=', 'c')))
+print(lab.solve('x', (('a', '-', 'x'), '=', 'c')))
 #  ('x', '=', ('a', '-', 'c'))  2 points
 
-print(solve('x', (('x', '-', 'b'), '=', 'c')))
+print(lab.solve('x', (('x', '-', 'b'), '=', 'c')))
 #  ('x', '=', ('c', '+', 'b'))  2 points
 
-print(solve('x', (('a', '*', 'x'), '=', 'c')))
+print(lab.solve('x', (('a', '*', 'x'), '=', 'c')))
 #  ('x', '=', ('c', '/', 'a'))  2 points
 
-print(solve('x', (('x', '*', 'b'), '=', 'c')))
+print(lab.solve('x', (('x', '*', 'b'), '=', 'c')))
 #  ('x', '=', ('c', '/', 'b'))  2 points
 
-print(solve('x', (('a', '/', 'x'), '=', 'c')))
+print(lab.solve('x', (('a', '/', 'x'), '=', 'c')))
 #  ('x', '=', ('a', '/', 'c'))  2 points
 
-print(solve('x', (('x', '/', 'b'), '=', 'c')))
+print(lab.solve('x', (('x', '/', 'b'), '=', 'c')))
 #  ('x', '=', ('c', '*', 'b'))  2 points
 
-print(solve('y', ('y', '=', (('m', '*', 'x'), '+', 'b'))))
+print(lab.solve('y', ('y', '=', (('m', '*', 'x'), '+', 'b'))))
 # ('y', '=', (('m', '*', 'x'), '+', 'b'))  2 points
 
-print(solve('x', ('y', '=', (('m', '*', 'x'), '+', 'b'))))
+print(lab.solve('x', ('y', '=', (('m', '*', 'x'), '+', 'b'))))
 # ('x', '=', (('y', '-', 'b'), '/', 'm'))  2 points
 
-print(solve('a', (('b', '+', 'c'), '=', ('d', '*', (('a', '/', 'e'), '-', 'f')))))
+print(lab.solve('a', (('b', '+', 'c'), '=', ('d', '*', (('a', '/', 'e'), '-', 'f')))))
 # ('a', '=', (((('b', '+', 'c'), '/', 'd'), '+', 'f'), '*', 'e'))  5 points

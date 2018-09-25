008e66814396b5ee3560065917744c6bae32e030
biedn001 <biedn001@umn.edu>
2018-09-24 22:30:49 -0500

    Finished lab 3

---
+++ Lab 3/lab.py
@@ -0,0 +1,42 @@
+def Sort(t):
+    if not t:
+        return t
+    else:
+        return Sort(Remove(t, Maximum(t))) + (Maximum(t),)
+
+def Maximum(t):
+    if len(t) == 1:
+        return t[0]
+    elif t[0] <= t[1]:
+        return Maximum(Remove(t, t[0]))
+    elif t[0] > t[1]:
+        return Maximum(Remove(t, t[1]))
+        
+def Remove(t, e):
+    return t[:t.index(e)] + t[t.index(e) + 1:] if t else t
+
+# Tests and return values are below
+
+print(Maximum((1,)))                      #  1                            2 pt.
+print(Maximum((1, 2)))                    #  2                            2 pt.
+print(Maximum((1, 1)))                    #  1                            2 pt.
+print(Maximum((1, 2, 3)))                 #  3                            2 pt.
+
+print(Remove((), 1))                      #  ()                           2 pt.
+print(Remove((1,), 1))                    #  ()                           2 pt.
+print(Remove((0, 1), 0))                  #  (1,)                         2 pt.
+print(Remove((0, 1, 2, 1, 3), 1))         #  (0, 2, 1, 3)                 2 pt.
+print(Remove((0, 1, 2, 1, 3), 2))         #  (0, 1, 1, 3)                 2 pt.
+print(Remove((1, 2, 3), 3))               #  (1, 2)                       2 pt.
+
+print(Sort(()))                           #  ()                           2 pt.
+print(Sort((0,)))                         #  (0,)                         2 pt.
+print(Sort((0, 1)))                       #  (0, 1)                       2 pt.
+print(Sort((1, 0)))                       #  (0, 1)                       2 pt.
+print(Sort((0, 0, 1)))                    #  (0, 0, 1)                    2 pt.
+print(Sort((0, 1, 0)))                    #  (0, 0, 1)                    2 pt.
+print(Sort((0, 0, 1)))                    #  (0, 0, 1)                    2 pt.
+
+print(Sort((9, 8, 7, 6, 5, 4, 3, 2, 1)))  #  (1, 2, 3, 4, 5, 6, 7, 8, 9)  2 pt.
+print(Sort((1, 2, 3, 4, 5, 6, 7, 8, 9)))  #  (1, 2, 3, 4, 5, 6, 7, 8, 9)  2 pt.
+print(Sort((1, 2, 1, 4, 2, 5, 4, 5, 3)))  #  (1, 1, 2, 2, 3, 4, 4, 5, 5)  2 pt.

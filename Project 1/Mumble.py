e90d10c7bd337d9b2336d8fddc7d3a3bf93fd3ca
Alex Biedny <abiedny09@gmail.com>
2018-10-12 13:31:27 -0500

    comment cleanup and print tweaks

--- Project 1/Mumble.py
+++ Project 1/Mumble.py
@@ -18,7 +18,6 @@ class Mumble:
         letArr = self.choosers[lastLetter][1:]
         randVal = self.rng.choose(self.choosers[lastLetter][0])
 
-        #get ready ready for code spaghetti
         for i, v in enumerate(letArr):
             if type(v) == int:
                 randVal -= v
@@ -30,14 +29,14 @@ class Mumble:
                 return #when we get here its time to unclutter the callstack
 
     def test(self, count, min, max):
-        #(10 points.) Print count random words, one per line, by calling make. Each word must have at least min letters, 
-        # and at most max letters. You must raise an exeption if 1 ≤ min ≤ max is not true. 
+        if min < 1 or max < min:
+            raise ValueError
         for ind in range(0, count):
             while True:
                 word = self.make(max)
                 if len(word) >= min:
                     break
-            print(word + "\n")
+            print(word)
 
 mumbler = Mumble(101, ch.choosers)
 mumbler.test(10, 5, 10)

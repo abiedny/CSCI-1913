190f992786a924439f27f152bf9d32dbf38b835c
Alex Biedny <abiedny09@gmail.com>
2018-10-23 17:19:46 -0500

    Issue #7 close. End of lab 6.

--- Lab 6/src/BinaryVsLinear.java
+++ Lab 6/src/BinaryVsLinear.java
@@ -1,6 +1,5 @@
 public class BinaryVsLinear
 {
-
     private static int linearSearch(int key, int[] keys)
     {
         int i = 0;
@@ -19,9 +18,10 @@ public class BinaryVsLinear
         int low = 0;
         int high = keys.length - 1;
         int i = 0;
+        int middle;
 
-        while (high > low) {
-            int middle = (low + high)/2;
+        while (low <= high) {
+            middle = (low + high)/2;
             if (keys[middle] == key) {
                 return i + 1;
             }
@@ -33,7 +33,7 @@ public class BinaryVsLinear
             }
             i++;
         }
-        return 0;
+        return -1;
     }
 
     public static void main(String[] args)
@@ -60,3 +60,36 @@ public class BinaryVsLinear
         }
     }
 }
+
+/* Program will output:
+1 1.0 1.0
+2 1.5 1.5
+3 2.0 1.6666666666666667
+4 2.5 2.0
+5 3.0 2.2
+6 3.5 2.3333333333333335
+7 4.0 2.4285714285714284
+8 4.5 2.625
+9 5.0 2.7777777777777777
+10 5.5 2.9
+11 6.0 3.0
+12 6.5 3.0833333333333335
+13 7.0 3.1538461538461537
+14 7.5 3.2142857142857144
+15 8.0 3.2666666666666666
+16 8.5 3.375
+17 9.0 3.4705882352941178
+18 9.5 3.5555555555555554
+19 10.0 3.6315789473684212
+20 10.5 3.7
+21 11.0 3.761904761904762
+22 11.5 3.8181818181818183
+23 12.0 3.869565217391304
+24 12.5 3.9166666666666665
+25 13.0 3.96
+26 13.5 4.0
+27 14.0 4.037037037037037
+28 14.5 4.071428571428571
+29 15.0 4.103448275862069
+30 15.5 4.133333333333334
+ */
---
+++ Lab 6/src/Questions.md
@@ -0,0 +1,7 @@
+## Based on your graph, for what array sizes is linear search more efficient than binary search?
+    Linear search appears to never be more efficient than binary search. For arrays of size 1, the efficiency is the same, 
+    however for every other array size, binary is more efficient.
+
+## Based on your graph, for what array sizes is binary search more efficient than linear search?
+    Binary search appears to be the most efficient for every array size. It has the same efficieny for an array of size 1,
+    but otherwise binary search is always faster.
